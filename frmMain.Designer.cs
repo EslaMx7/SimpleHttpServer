03ee7c348b74d33981a4fb468c57eda5efed5f6c
eslamx7 <eslamx7@gmail.com>
2014-01-10 21:06:25 +0200

    Initial Commit,Initial Version

---
+++ frmMain.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SimpleServers
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpHttp = new System.Windows.Forms.GroupBox();
+            this.txtHttpLog = new System.Windows.Forms.TextBox();
+            this.btnStartHttpServ = new System.Windows.Forms.Button();
+            this.txtHttpPort = new System.Windows.Forms.NumericUpDown();
+            this.lblHPort = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.lnkSite = new System.Windows.Forms.LinkLabel();
+            this.gpHttp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtHttpPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gpHttp
+            // 
+            this.gpHttp.Controls.Add(this.checkBox1);
+            this.gpHttp.Controls.Add(this.txtHttpLog);
+            this.gpHttp.Controls.Add(this.btnStartHttpServ);
+            this.gpHttp.Controls.Add(this.txtHttpPort);
+            this.gpHttp.Controls.Add(this.lblHPort);
+            this.gpHttp.Location = new System.Drawing.Point(19, 30);
+            this.gpHttp.Name = "gpHttp";
+            this.gpHttp.Size = new System.Drawing.Size(393, 386);
+            this.gpHttp.TabIndex = 0;
+            this.gpHttp.TabStop = false;
+            this.gpHttp.Text = "HTTP Server";
+            // 
+            // txtHttpLog
+            // 
+            this.txtHttpLog.Location = new System.Drawing.Point(7, 111);
+            this.txtHttpLog.Multiline = true;
+            this.txtHttpLog.Name = "txtHttpLog";
+            this.txtHttpLog.ReadOnly = true;
+            this.txtHttpLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtHttpLog.Size = new System.Drawing.Size(374, 269);
+            this.txtHttpLog.TabIndex = 3;
+            // 
+            // btnStartHttpServ
+            // 
+            this.btnStartHttpServ.Location = new System.Drawing.Point(278, 30);
+            this.btnStartHttpServ.Name = "btnStartHttpServ";
+            this.btnStartHttpServ.Size = new System.Drawing.Size(103, 23);
+            this.btnStartHttpServ.TabIndex = 2;
+            this.btnStartHttpServ.Text = "Start Server";
+            this.btnStartHttpServ.UseVisualStyleBackColor = true;
+            this.btnStartHttpServ.Click += new System.EventHandler(this.btnStartHttpServ_Click);
+            // 
+            // txtHttpPort
+            // 
+            this.txtHttpPort.Location = new System.Drawing.Point(92, 33);
+            this.txtHttpPort.Maximum = new decimal(new int[] {
+            65000,
+            0,
+            0,
+            0});
+            this.txtHttpPort.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.txtHttpPort.Name = "txtHttpPort";
+            this.txtHttpPort.Size = new System.Drawing.Size(64, 20);
+            this.txtHttpPort.TabIndex = 1;
+            this.txtHttpPort.Value = new decimal(new int[] {
+            81,
+            0,
+            0,
+            0});
+            // 
+            // lblHPort
+            // 
+            this.lblHPort.AutoSize = true;
+            this.lblHPort.Location = new System.Drawing.Point(7, 35);
+            this.lblHPort.Name = "lblHPort";
+            this.lblHPort.Size = new System.Drawing.Size(79, 13);
+            this.lblHPort.TabIndex = 0;
+            this.lblHPort.Text = "Listening Port :";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(179, 34);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Enable PHP";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // lnkSite
+            // 
+            this.lnkSite.AutoSize = true;
+            this.lnkSite.Location = new System.Drawing.Point(336, 419);
+            this.lnkSite.Name = "lnkSite";
+            this.lnkSite.Size = new System.Drawing.Size(76, 13);
+            this.lnkSite.TabIndex = 1;
+            this.lnkSite.TabStop = true;
+            this.lnkSite.Text = "© eslamx.com";
+            this.lnkSite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkSite_LinkClicked);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 436);
+            this.Controls.Add(this.lnkSite);
+            this.Controls.Add(this.gpHttp);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple Servers v1 | by : Eslam Hamouda";
+            this.gpHttp.ResumeLayout(false);
+            this.gpHttp.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtHttpPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpHttp;
+        private System.Windows.Forms.Button btnStartHttpServ;
+        private System.Windows.Forms.NumericUpDown txtHttpPort;
+        private System.Windows.Forms.Label lblHPort;
+        private System.Windows.Forms.TextBox txtHttpLog;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.LinkLabel lnkSite;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleServers
+{
+    public partial class frmMain : Form
+    {
+        HttpListener HttpServ;
+        string PhpCompilerPath = @"D:\Projects\Visual Studio 2013\Projects\TestSolution\SimpleServers\bin\Debug\php\v5.3\php.exe";
+        public frmMain()
+        {
+            InitializeComponent();
+            HttpServ = new HttpListener();
+        }
+
+
+        public string ProcessPhpPage(string phpCompilerPath, string pageFileName)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = phpCompilerPath;
+            proc.StartInfo.Arguments = "-d \"display_errors=1\" -d \"error_reporting=E_PARSE\" \"" + pageFileName + "\"";
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.RedirectStandardError = true;
+            proc.Start();
+            string res = proc.StandardOutput.ReadToEnd();
+            if (string.IsNullOrEmpty(res))
+            {
+                res = proc.StandardError.ReadToEnd();
+                res = "<h2 style=\"color:red;\">Error!</h2><hr/> <h4>Error Details :</h4> <pre>" + res + "</pre>";
+                proc.StandardError.Close();
+            }
+            if (res.StartsWith("\nParse error: syntax error"))
+                res = "<h2 style=\"color:red;\">Error!</h2><hr/> <h4>Error Details :</h4> <pre>" + res+"</pre>";
+
+
+            proc.StandardOutput.Close();
+           
+            proc.Close();
+            return res;
+        }
+
+        private async void btnStartHttpServ_Click(object sender, EventArgs e)
+        {
+            if (HttpServ.IsListening)
+            {
+                try
+                {
+                    HttpServ.Stop();
+                    // HttpServ.Abort();
+                }
+                catch
+                {
+                    txtHttpLog.Text += "Server has been Shutdown.\r\n";
+                }
+                btnStartHttpServ.Text = "Start Server";
+            }
+            else
+            {
+                await StartServer();
+            }
+        }
+
+        private async Task StartServer()
+        {
+            HttpServ = new HttpListener();
+            btnStartHttpServ.Text = "Stop Server";
+            HttpServ.Prefixes.Add("http://localhost:" + txtHttpPort.Value.ToString() + "/");
+            HttpServ.Start();
+            txtHttpLog.Text += "Start Listining on Port :" + txtHttpPort.Value + "\r\n";
+            txtHttpLog.Text += "Server is Running...\r\n\r\n";
+            while (true)
+            {
+                try
+                {
+                    var ctx = await HttpServ.GetContextAsync();
+
+
+                    txtHttpLog.Text += "Request: " + ctx.Request.Url.AbsoluteUri + "\r\n";
+                    var page = Application.StartupPath + "\\wwwroot" + ctx.Request.Url.LocalPath;
+                    if (File.Exists(page))
+                    {
+                        string file;
+                        var ext = new FileInfo(page);
+                        if (ext.Extension == ".php")
+                        {
+                            file = ProcessPhpPage(PhpCompilerPath, page);
+                            txtHttpLog.Text += "Processing php page...\r\n";
+                        }
+                        else
+                        {
+                            file = File.ReadAllText(page);
+                        }
+
+                        await ctx.Response.OutputStream.WriteAsync(ASCIIEncoding.UTF8.GetBytes(file), 0, file.Length);
+                        ctx.Response.OutputStream.Close();
+                        ctx.Response.Close();
+                        txtHttpLog.Text += "Response: 200 OK\r\n\r\n";
+
+                    }
+                    else
+                    {
+                        ctx.Response.StatusCode = 404;
+                        var file = "<h2 style=\"color:red;\">404 File Not Found !!!</h2>";
+                        await ctx.Response.OutputStream.WriteAsync(ASCIIEncoding.UTF8.GetBytes(file), 0, file.Length);
+                        ctx.Response.OutputStream.Close();
+
+                        ctx.Response.Close();
+                        txtHttpLog.Text += "Response: 404 Not Found\r\n\r\n";
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    txtHttpLog.Text += "\r\nException: Server Stopped!!!\r\n\r\n";
+                   // txtHttpLog.Text += "\r\nException: " + ex.Message + "\r\n\r\n";
+                    break;
+                }
+                txtHttpLog.Select(0, 0);
+            }
+
+
+        }
+
+        private void lnkSite_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://eslamx.com");
+        }
+
+    }
+}
